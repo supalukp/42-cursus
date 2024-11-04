43fdb6dd43a229c627f2f59e32a33e3468e1dcca
supalukp <supaluk.pun@gmail.com>
2024-11-04 14:49:42 +0100

    ADD : ft_isalnum.c

---
+++ libft/ft_isalnum.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+int isalnum(int c)
+{
+    if (isalpha(c) || isdigit(c))
+        return (1);
+    return (0);
+}
+
+/*#include <stdio.h>
+
+int main()
+{
+    int c = '/';
+    printf("%d", isalnum(c));
+    
+}*/
