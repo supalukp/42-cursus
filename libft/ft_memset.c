be45906345cd7bc056e164c3ac5d96b08475a909
supalukp <supaluk.pun@gmail.com>
2024-11-04 17:57:38 +0100

    UPDATE : ft_memset.c = add more test to be more understand

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -12,26 +12,25 @@ void *ft_memset(void *s, int c, size_t n)
     return s;
 }
 
-/*
 #include <stdio.h>
-
-void printArray(int arr[], int n)
-{
-   for (int i=0; i<n; i++)
-      printf("%d ", arr[i]);
-}
 
 int main ()
 {
-    char test[10] = "HelloWorld";
+    /*char test[10] = "HelloWorld";
     ft_memset(test + 5, '.', sizeof(char) * 3);
     for (int i = 0; i < 10; i++)
     {
         printf("%c", test[i]);
+    }*/
+
+    int arr[2] = {0, 145232};
+
+    ft_memset(arr, 65, 6);
+    //printArray(arr, 2);
+
+    char *p = arr;
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%d ", p[i]);
     }
-
-    int arr[10];
-
-    ft_memset(arr, 0, sizeof(int) * 10);
-    printArray(arr, 10);
-}*/
+}
