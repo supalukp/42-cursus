aeaf4872c746833167e1be3af7485cf1df4e592f
supalukp <supaluk.pun@gmail.com>
2024-11-05 16:35:29 +0100

    feat : add ft_strlcat.c

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,35 @@
+#include "libft.h"
+
+size_t ft_strlcat(char *dst, const char *src, size_t size)
+{
+    size_t i;
+    size_t dst_len;
+    size_t src_len;
+
+    i = 0;
+    dst_len = 0;
+    src_len = 0;
+    while (dst[dst_len])
+        dst_len++;
+    while (src[src_len])
+        src_len++;
+    if (size <= dst_len)
+        return (size + src_len);
+    while (src[i] && i < (size - dst_len - 1))
+    {
+        dst[dst_len + i] = src[i];
+        i++;
+    }
+    dst[dst_len + i] = '\0';
+    return (dst_len + src_len);
+}
+
+// int main()
+// {
+//     char a[] = "hello";
+//     char b[10] = "test";
+//     printf("b before: %s\n", b);
+//     size_t i = ft_strlcat(b, a, 9);
+//     printf("%ld\n", i);
+//     printf("%s\n", b);
+// }
