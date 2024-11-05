9641d959aae4f4ce8e7dd9b42578ccf09e0ad98c
supalukp <supaluk.pun@gmail.com>
2024-11-05 10:19:52 +0100

    fix : add test in main

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -9,22 +9,27 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
     unsigned const char *t = src;
     while (n > 0)
     {
-        *p = *t;
-        n--;
-        p++;
+        *p= *t;
         t++;
+        p++;
+        n--;
     }
     return (dest);
 }
 
-/*#include <stdio.h>
-#include <string.h>
+//#include <string.h>
 
-int main()
-{
-    const char src[] = "Hello";
-    char dest[5] = "0";
-    //memcpy(dest, src, 5);
-    ft_memcpy(dest, src, 3);
-    printf("%s", dest);
-}*/
+// int main()
+// {
+    // const char src[] = "Hello";
+    // char dest[5] = "0";
+
+    // ft_memcpy(dest, src, 6);
+    // printf("ft_memcpy : %s\n", dest);
+
+//     const char src1[] = "Hello";
+//     char dest1[6] = "0";
+
+//     memcpy(dest1, src1, 5);
+//     printf("memcpy : %s\n", dest1);
+// }
