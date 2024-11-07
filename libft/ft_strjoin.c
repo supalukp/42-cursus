028c73bc8acf535b68dd70d942d57f9bae76cbd1
supalukp <supaluk.pun@gmail.com>
2024-11-07 16:18:25 +0100

    feat : ft_strjoin.c

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rose <rose@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 15:39:51 by rose              #+#    #+#             */
+/*   Updated: 2024/11/07 16:16:42 by rose             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*result;
+
+	result = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		result[i] = s2[j];
+		i++;
+		j++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+// int	main(void)
+// {
+// 	char s1[] = "Hello";
+// 	char s2[] = "World";
+// 	char *result = ft_strjoin(s1, s2);
+// 	printf("%s\n", result);
+// 	free(result);
+// }
