73c443522a0d2b99bbce613be9654cfd618c5d35
supalukp <supaluk.pun@gmail.com>
2024-11-11 15:40:50 +0100

    fix : compare type

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,28 +6,25 @@
 /*   By: rose <rose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 10:05:33 by rose              #+#    #+#             */
-/*   Updated: 2024/11/07 10:05:33 by rose             ###   ########.fr       */
+/*   Updated: 2024/11/11 15:40:06 by rose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//#include <string.h>
-
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (s1[i] == s2[i] && i < n - 1)
+	while ((s1[i] == s2[i]) && (i < n - 1))
 		i++;
 	return (s1[i] - s2[i]);
 }
 
-// int main()
+// int	main(void)
 // {
-//     char s1[] = "abdde";
-//     char s2[] = "abc";
-//     printf("%d\n", ft_strncmp(s1, s2, 4));
-
+// 	char s1[] = "abdde";
+// 	char s2[] = "abc";
+// 	printf("%d\n", ft_strncmp(s1, s2, 4));
 // }
