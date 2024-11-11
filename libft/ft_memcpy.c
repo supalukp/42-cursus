77356da3ebf0b30fd0c664e37337222b3766e514
supalukp <supaluk.pun@gmail.com>
2024-11-11 17:33:43 +0100

    fix : function

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,46 +6,37 @@
 /*   By: rose <rose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 10:04:43 by rose              #+#    #+#             */
-/*   Updated: 2024/11/07 10:52:23 by rose             ###   ########.fr       */
+/*   Updated: 2024/11/11 17:33:13 by rose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// The memcpy() function copies n bytes from memory area 
-//src to memory area dest. The memory areas must not overlap
+// The memcpy() function copies n bytes from memory area
+// src to memory area dest. The memory areas must not overlap
 // returns a pointer to dest.
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned char		*p;
-	unsigned const char	*t;
+	char		*c_dest;
+	const char	*c_src;
+	size_t		i;
 
-	p = dest;
-	t = src;
-	while (n > 0)
+	c_dest = (char *)dest;
+	c_src = (char *)src;
+	i = 0;
+	while (i < n)
 	{
-		*p = *t;
-		t++;
-		p++;
-		n--;
+		c_dest[i] = c_src[i];
+		i++;
 	}
 	return (dest);
 }
 
-//#include <string.h>
-
-// int main()
+// int	main(void)
 // {
-// const char src[] = "Hello";
-// char dest[5] = "0";
-
-// ft_memcpy(dest, src, 6);
-// printf("ft_memcpy : %s\n", dest);
-
-//     const char src1[] = "Hello";
-//     char dest1[6] = "0";
-
-//     memcpy(dest1, src1, 5);
-//     printf("memcpy : %s\n", dest1);
+// 	char str1[] = "Hello, world!";
+// 	ft_memcpy(str1 + 7, str1, 5);
+// 	printf("memcpy: %s\n", str1);
+// 	return (0);
 // }
