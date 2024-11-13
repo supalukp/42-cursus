ae37ba6602acc40df3d51cc510f17e6c5270bc5d
supalukp <supaluk.pun@gmail.com>
2024-11-13 14:50:52 +0100

    fix : fix bugging

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: rose <rose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 10:05:40 by rose              #+#    #+#             */
-/*   Updated: 2024/11/11 10:31:16 by rose             ###   ########.fr       */
+/*   Updated: 2024/11/13 14:50:11 by rose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 
 	i = 0;
 	j = 0;
-	if (little == NULL)
+	if (little == NULL || *little == '\0')
 		return ((char *)big);
-	while (big[i] && i < len - 1)
+	while (big[i] && i < len)
 	{
-		while (big[i + j] == little[j])
+		while (big[i + j] == little[j] && (i + j < len))
 		{
-			if (little[j + 1] == '\0')
+			if (little[j + 1] == '\0' )
 				return ((char *)big + i);
 			j++;
 		}
@@ -37,9 +37,11 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 
 // int main()
 // {
-//     const char *hay = "Hello, world!";
-//     const char *needle = "ow";
+//     // const char *hay = "Hello, world!";
+//     // const char *needle = "ow";
 
-//     char *result = ft_strnstr(hay, needle, 12);
-//     printf("%s", result);
+//     // char *result = ft_strnstr(hay, needle, 12);
+//     // printf("%s", result);
+	
+// 	printf("%s", ft_strnstr("fake", ((void*)0), 3));
 // }
